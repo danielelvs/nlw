714fee8e073f9acb6ce17a06fccc4164b6b963b0
Daniele Santos <daniele.santos@trustly.com>
2021-06-23 22:49:37 -0300

    [NLW - ReactJS] Aula Prática 02 - Conteúdo que faltava

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,48 @@
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import { useHistory } from "react-router-dom";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/auth.scss";
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push("/rooms/new");
+  }
   return (
-    <div>
+    <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie salas de Q&smp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiêecia em tempo-real</p>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
-          <img src={logoImg} alt="Letkeask" />
-          <button>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Cria sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
-            <input
-              type="text"
-              placeholder="Digite o código da sala"
-            />
-            <button type="submit">
-              Entrar na sala
-            </button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
-
       </main>
     </div>
-  )
+  );
 }
-
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,36 +1,32 @@
-// import { useContext } from 'react'
-// import { AuthContext } from '../contexts/AuthContext'
-import { Link } from 'react-router-dom'
-import { useAuth } from '../hooks/useAuth'
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { Button } from "../components/Button";
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
 
-import { Button } from '../components/Button'
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-
-import '../styles/auth.scss'
+import "../styles/auth.scss";
 
 export function NewRoom() {
-  const { user } = useAuth()
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiêecia em tempo-real</p>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
         <div className="main-content">
-          <img src={logoImg} alt="Letkeask" />
+          <img src={logoImg} alt="Letmeask" />
           <h1>{user?.name}</h1>
           <h2>Crie uma nova sala</h2>
           <form>
-            <input
-              type="text"
-              placeholder="Nome da sala"
-            />
-            <Button type="submit">
-              Criar sala
-            </Button>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
             Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
@@ -38,6 +34,5 @@ export function NewRoom() {
         </div>
       </main>
     </div>
-  )
+  );
 }
-
